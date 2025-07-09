fc73536f591ea4f28148e0f93733676f688ab3f6
n00bmaster <satty9509@gmail.com>
2025-07-10 03:43:50 +0530

    added cli args

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module weather-cli
 
 go 1.24
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
+func loadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+}
+
 func main() {
-	res, err := http.Get("https://api.weatherapi.com/v1/current.json?key=b3f8320ed9d842de890213957250907&q=London&aqi=no")
+	loadEnv()
+
+	apiKey := os.Getenv("API_KEY")
+	cityName := os.Getenv("CITY_NAME")
+
+	if len(os.Args) >= 2 {
+		cityName = os.Args[1]
+	}
+
+	res, err := http.Get("https://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + cityName + "&aqi=no")
 	if err != nil {
 		panic(err)
 	}
